aeee330066660b220e872a7bc57c71ca55302b21
Beicker Zambrano <beicker998@gmail.com>
2024-10-19 07:38:52 -0500

    Upgrade to Plasma 6 and add Nvidia drivers among others

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -50,7 +50,9 @@
   # Enable the KDE Plasma Desktop Environment.
    # services.xserver.displayManager.sddm.enable = true; -- deprecated
 	 services.displayManager.sddm.enable = true;
-	 services.xserver.desktopManager.plasma5.enable = true;
+	 services.displayManager.defaultSession = "plasma";
+	 services.displayManager.sddm.wayland.enable = true;
+	 services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -67,7 +69,6 @@
 	];
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -81,6 +82,44 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+		      # write extra config
+      extraConfig.pipewire = {
+        "99-lowlatency" = {
+          context = {
+            properties.default.clock.min-quantum = 32;
+            modules = [
+              {
+                name = "libpipewire-module-rtkit";
+                flags = ["ifexists" "nofail"];
+                args = {
+                  nice.level = -15;
+                  rt = {
+                    prio = 88;
+                    time.soft = 200000;
+                    time.hard = 200000;
+                  };
+                };
+              }
+              {
+                name = "libpipewire-module-protocol-pulse";
+                args = {
+                  server.address = ["unix:native"];
+                  pulse.min = {
+                    req = "32/96000";
+                    quantum = "32/96000";
+                    frag = "32/96000";
+                  };
+                };
+              }
+            ];
+
+            stream.properties = {
+              node.latency = "32/96000";
+              resample.quality = 1;
+            };
+          };
+        };
+      };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -138,62 +177,21 @@
 		options = "--delete-older-than 30d";
 	};
 
-
-# 	# Enable OpenGL
-#   hardware.opengl = {
-#     enable = true;
-#     driSupport = true;
-#     driSupport32Bit = true;
-#   };
-#
-# # Load nvidia driver for Xorg and Wayland
-#   services.xserver.videoDrivers = ["nvidia"];
-#   
-#   hardware.nvidia.modesetting.enable = true;
-#   hardware.nvidia.powerManagement.enable = false;
-#   hardware.nvidia.powerManagement.finegrained = false;
-#   hardware.nvidia.open = false;
-#   hardware.nvidia.nvidiaSettings = true;
-# # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
-#   hardware.nvidia.package = let 
-#   rcu_patch = pkgs.fetchpatch {
-#     url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-#     hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-#   };
-# in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-#     version = "535.154.05";
-#     sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-#     sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-#     openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-#     settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-#     persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-#
-#     #version = "550.40.07";
-#     #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-#     #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-#     #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-#     #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-#     #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-#
-#     patches = [ rcu_patch ];
-#  };
-
   # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+	hardware.graphics = {
+		enable = true;
+		enable32Bit = true;
+	};
 	hardware.opentabletdriver.enable = true;
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-
+		open = false;
     nvidiaSettings = true;
 		modesetting.enable = true;
     # # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
